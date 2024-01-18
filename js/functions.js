f5a987656e71852d56858e3c3255f25e3b0d9793
diana56178 <yusupova.diana.94@mail.ru>
2024-01-18 12:23:17 +0500

    исправление функций

--- js/functions.js
+++ js/functions.js
@@ -10,37 +10,31 @@ function getNumber(string) {
 
 getNumber();
 
-
 function getRandomNumber(min, max, count) {
-  if (isNaN(min && max)) {
-    return NaN;
-  }
-  let rand = Math.abs(min) + Math.random() * (Math.abs(max) + 1 - Math.abs(min));
-  rand = rand.toFixed(count);
-  return Number(rand);
+  const rand = Math.abs(min) + Math.random() * (Math.abs(max) + 1 - Math.abs(min));
+  return Number(rand.toFixed(count));
 }
 
 getRandomNumber();
-
 function isPalindrome(string) {
-  if (string.toString().split('').reverse().join('') === string.toString()) {
-    return true;
-  }
-
-  return false;
+  string = string.toLowerCase().split('');
+  return string.reverse().join('') === string.join('');
 }
 
 isPalindrome();
 
-//Функция, которая принимает три параметра: исходную строку, минимальную длину и строку
-//с добавочными символами — и возвращает исходную строку,
-//дополненную указанными символами до заданной длины. Символы добавляются в начало строки.
-//Если исходная строка превышает заданную длину, она не должна обрезаться.
-//Если «добивка» слишком длинная, она обрезается с конца.
+function padStart(rawString, size, template) {
+  if (rawString.length > size) {
+    return rawString;
+  }
 
-// function createString(string, min, newStr) {
+  while (rawString.length <= size) {
+    if (rawString.length + template.length > size) {
+      return template.slice(0, Math.abs(rawString.length - size)) + rawString;
+    }
+    rawString = template + rawString;
+  }
+  return rawString;
+}
 
-
-// }
-
-// generateString();
+padStart();
