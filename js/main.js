284a199766200692498748458d8ab309a22b67c0
diana56178 <yusupova.diana.94@mail.ru>
2024-02-01 10:29:24 +0500

    внесла правки после проверки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const TITLES = [
+const titles = [
   'Olympus Hotel By Umbrella',
   'Apart Hotel Console',
   'Hotel Sky Station',
@@ -11,7 +11,7 @@ const TITLES = [
   'Vintage Hotel',
 ];
 
-const TYPES = [
+const types = [
   'palace',
   'flat',
   'house',
@@ -19,13 +19,13 @@ const TYPES = [
   'hotel',
 ];
 
-const CHECKIN_TIMES = [
+const checkinTimes = [
   '12:00',
   '13:00',
   '14:00',
 ];
 
-const FEATURES = [
+const features = [
   'wifi',
   'dishwasher',
   'parking',
@@ -34,7 +34,7 @@ const FEATURES = [
   'conditioner',
 ];
 
-const DESCRIPTIONS = [
+const descriptions = [
   'Апарт-отель VovaDoma с террасой и бесплатным Wi-Fi расположен в центре Тбилиси...',
   'Мягкие комнаты с наивысшей оценкой гостей города!',
   'Лучшее кофе в нашем лобби-баре!',
@@ -47,79 +47,56 @@ const DESCRIPTIONS = [
   'Нас выбираю все транзитные путешественники. Находимся рядом с автобусным и ЖД вокзалом!',
 ];
 
-const PHOTOS = [
+const photos = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
 function getRandomNumber(min, max, count = 0) {
-  const rand = Math.abs(min) + Math.random() * (Math.abs(max) - Math.abs(min));
-  return Number(rand.toFixed(count));
+  return Number((Math.abs(min) + Math.random() * (Math.abs(max) - Math.abs(min))).toFixed(count));
 }
-
-
-function createRandomIdFromRangeGenerator (min, max, count = 0) {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = getRandomNumber(min, max, count);
-    if (!count && previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomNumber(min, max, count);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-}
-
-function generateUrlId() {
-  const ID = createRandomIdFromRangeGenerator(1, 10);
-  return ID().toString().padStart(2, 0);
-}
-
-const generateLat = createRandomIdFromRangeGenerator(35.65, 35.7, 5);
-const generateLng = createRandomIdFromRangeGenerator(139.70000, 139.80000, 5);
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
 function generateRandomArray (someArr) {
   const arrCount = getRandomNumber(1, someArr.length - 1);
   const randomArr = [];
-  const generateArr = createRandomIdFromRangeGenerator(0, (someArr.length - 1));
+  const generateArr = getRandomNumber(0, (someArr.length - 1));
 
   for (let i = 0; i < arrCount; i++) {
-    randomArr.push(someArr[generateArr()]);
+    randomArr.push(someArr[generateArr]);
   }
 
   return randomArr;
 }
 
-const createRoom = () => ({
-  autor: {
-    avatar: `img/avatars/user-${generateUrlId()}.png`,
-  },
-  offer: {
-    title: getRandomArrayElement(TITLES),
-    address: `${generateLat()}, ${generateLng()}`,
-    getPrice: getRandomNumber(3500, 10000),
-    type: getRandomArrayElement(TYPES),
-    getRooms: getRandomNumber(1, 5),
-    guests: getRandomNumber(0, 5),
-    checkin: getRandomArrayElement(CHECKIN_TIMES),
-    checkout: getRandomArrayElement(CHECKIN_TIMES),
-    features: generateRandomArray(FEATURES),
-    description: getRandomArrayElement(DESCRIPTIONS),
-    photos: generateRandomArray(PHOTOS)
-  },
-  location: {
-    lat: generateLat(),
-    lng: generateLng(),
+const createOffer = (_, index) => {
+  const latitude = getRandomNumber(35.65, 35.7, 5);;
+  const longitude = getRandomNumber(139.70000, 139.80000, 5);
+  return {
+    autor: {
+      avatar: `img/avatars/user-${(index + 1).toString().padStart(2, 0)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(titles),
+      address: `${latitude}, ${longitude}`,
+      price: getRandomNumber(3500, 10000),
+      type: getRandomArrayElement(types),
+      roomCount: getRandomNumber(1, 5),
+      guestCount: getRandomNumber(0, 5),
+      checkin: getRandomArrayElement(checkinTimes),
+      checkout: getRandomArrayElement(checkinTimes),
+      features: generateRandomArray(features),
+      description: getRandomArrayElement(descriptions),
+      photos: generateRandomArray(photos)
+    },
+    location: {
+      latitude,
+      longitude
+    }
   }
-});
+};
 
-const createRooms = (length) => Array.from({ length }, createRoom);
+const createOffers = (offersCount) => Array.from({length: offersCount}, createOffer);
 
-createRooms();
